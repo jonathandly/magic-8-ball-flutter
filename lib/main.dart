ea8935f14fe0b039867c72ad6bc648a6d7031850
Jonathan Daly <dalyj138@gmail.com>
2020-06-10 21:27:07 -0400

    created classes for BallPage, Ball, and _BallState. Added package random_color to create unsurprisingly random colors.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:random_color/random_color.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask me anything!'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    RandomColor _randomColor = RandomColor();
+    return Container(
+      color: _randomColor.randomColor(),
+      child: Row(
+        children: [
+          Expanded(
+            child: Center(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    ballNumber =
+                        Random(DateTime.now().millisecond).nextInt(5) + 1;
+                    print('ballNumber == $ballNumber');
+                  });
+                },
+                child: Image.asset('images/ball$ballNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
